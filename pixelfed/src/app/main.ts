e758f95e2ecc99551ce90631410da65fb5e77d46
alexayers <ayers.alex@gmail.com>
2023-07-26 11:48:22 -0400

    Initial commit

--- pixelfed/src/app/main.ts
+++ pixelfed/src/app/main.ts
@@ -1,21 +1,21 @@
 import config from "../cfg/config.json";
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 import Mastodon from 'mastodon-api';
 import * as fs from "fs";
 
 (async () => {
 
     console.log("Running...");
-    const PIXELFED_API = config.pixelFed.baseURL;
-    const PIXELFED_STORAGE_ROOT = config.pixelFed.baseStorage;
+    const PIXELFED_API : string = config.pixelFed.baseURL;
+    const PIXELFED_STORAGE_ROOT : string = config.pixelFed.baseStorage;
 
-    for (let accountIdx = 0; accountIdx < config.pixelFed.sync.length; accountIdx++) {
+    for (let accountIdx : number = 0; accountIdx < config.pixelFed.sync.length; accountIdx++) {
 
-        const MASTODON_API = config.pixelFed.sync[accountIdx]["to"]["api"];
-        const accountID = config.pixelFed.sync[accountIdx].from["account"];
-        const response = await axios.get(`${PIXELFED_API}/${accountID}/statuses?limit=1`)
+        const MASTODON_API : string = config.pixelFed.sync[accountIdx]["to"]["api"];
+        const accountID : string = config.pixelFed.sync[accountIdx].from["account"];
+        const response : AxiosResponse<any,any> = await axios.get(`${PIXELFED_API}/${accountID}/statuses?limit=1`)
         const imageText = response.data[0]["content_text"];
-        const imageURL = PIXELFED_STORAGE_ROOT + response.data[0]["media_attachments"][0]["url"].split("storage")[1];
+        const imageURL : string = PIXELFED_STORAGE_ROOT + response.data[0]["media_attachments"][0]["url"].split("storage")[1];
 
         fs.open(accountID,'r',function(err, fd){
             if (err) {
@@ -42,7 +42,7 @@ import * as fs from "fs";
         M.post('media', {file: fs.createReadStream(imageURL)}).then(resp => {
             const id = resp.data.id;
 
-            let contents = fs.readFileSync(accountID, {encoding: 'utf8', flag: 'r'});
+            let contents: string = fs.readFileSync(accountID, {encoding: 'utf8', flag: 'r'});
 
             if (contents != imageURL) {
                 console.log(`Sending image with status ${imageText}`);
