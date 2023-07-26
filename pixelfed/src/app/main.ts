8bd6a877e2d2d9072c6207b224d471a1090269bf
alexayers <ayers.alex@gmail.com>
2023-07-26 11:30:48 -0400

    Initial commit

--- pixelfed/src/app/main.ts
+++ pixelfed/src/app/main.ts
@@ -9,33 +9,37 @@ import * as fs from "fs";
     const PIXELFED_API = config.pixelFed.baseURL;
     const PIXELFED_STORAGE_ROOT = config.pixelFed.baseStorage;
 
-    const MASTODON_API = config.pixelFed.sync[0]["to"]["api"];
-    const accountID = config.pixelFed.sync[0].from["account"];
-    const response = await axios.get(`${PIXELFED_API}/${accountID}/statuses?limit=1`)
-    const imageText = response.data[0]["content_text"];
-    const imageURL = PIXELFED_STORAGE_ROOT + response.data[0]["media_attachments"][0]["url"].split("storage")[1];
+    for (let accountIdx = 0; accountIdx < config.pixelFed.sync.length; accountIdx++) {
+        const MASTODON_API = config.pixelFed.sync[accountIdx]["to"]["api"];
+        const accountID = config.pixelFed.sync[accountIdx].from["account"];
+        const response = await axios.get(`${PIXELFED_API}/${accountID}/statuses?limit=1`)
+        const imageText = response.data[0]["content_text"];
+        const imageURL = PIXELFED_STORAGE_ROOT + response.data[0]["media_attachments"][0]["url"].split("storage")[1];
 
-    console.log(`Running for account ${accountID} fetching ${imageURL}`);
+        console.log(`Running for account ${accountID} fetching ${imageURL}`);
 
-    const M = new Mastodon({
-        client_secret: config.pixelFed.sync[0]["to"]["clientSecret"],
-        client_key: config.pixelFed.sync[0]["to"]["clientKey"],
-        access_token: config.pixelFed.sync[0]["to"]["accessToken"],
-        timeout_ms: 60 * 1000,
-        api_url: MASTODON_API
-    });
+        const M = new Mastodon({
+            client_secret: config.pixelFed.sync[accountIdx]["to"]["clientSecret"],
+            client_key: config.pixelFed.sync[accountIdx]["to"]["clientKey"],
+            access_token: config.pixelFed.sync[accountIdx]["to"]["accessToken"],
+            timeout_ms: 60 * 1000,
+            api_url: MASTODON_API
+        });
 
-    console.log(`Authenticated against Mastodon ${MASTODON_API}`);
+        console.log(`Authenticated against Mastodon ${MASTODON_API}`);
 
-    M.post('media', {file: fs.createReadStream(imageURL)}).then(resp => {
-        const id = resp.data.id;
+        M.post('media', {file: fs.createReadStream(imageURL)}).then(resp => {
+            const id = resp.data.id;
 
-        console.log(`Sending image with status ${imageText}`);
+            console.log(`Sending image with status ${imageText}`);
 
-        M.post('statuses', {status: imageText, media_ids: [id]})
-    });
+            M.post('statuses', {status: imageText, media_ids: [id]})
+        });
 
-    console.log(`Posted media`);
+        console.log(`Posted media`);
+    }
+
+
 
 })()
 
