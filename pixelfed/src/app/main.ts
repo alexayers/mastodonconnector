27c6a628a11684fd0cb4293c6306d52c8f1cf5e2
alexayers <ayers.alex@gmail.com>
2023-07-26 11:11:44 -0400

    Initial commit

--- pixelfed/src/app/main.ts
+++ pixelfed/src/app/main.ts
@@ -5,14 +5,17 @@ import * as fs from "fs";
 
 (async () => {
 
+    console.log("Running...");
     const PIXELFED_API = config.pixelFed.baseURL;
     const PIXELFED_STORAGE_ROOT = config.pixelFed.baseStorage;
 
     const MASTODON_API = config.pixelFed.sync[0]["to"]["api"];
-    let accountID = config.pixelFed.sync[0].from["account"];
+    const accountID = config.pixelFed.sync[0].from["account"];
     const response = await axios.get(`${PIXELFED_API}/${accountID}/statuses?limit=1`)
-    let imageText = response.data[0]["content_text"];
-    let imageURL = PIXELFED_STORAGE_ROOT + response.data[0]["media_attachments"][0]["url"].split("storage")[1];
+    const imageText = response.data[0]["content_text"];
+    const imageURL = PIXELFED_STORAGE_ROOT + response.data[0]["media_attachments"][0]["url"].split("storage")[1];
+
+    console.log(`Running for account ${accountID} fetching ${imageURL}`);
 
     const M = new Mastodon({
         client_secret: config.pixelFed.sync[0]["to"]["clientSecret"],
@@ -22,10 +25,14 @@ import * as fs from "fs";
         api_url: MASTODON_API
     });
 
+    console.log(`Authenticated against Mastodon ${MASTODON_API}`);
+
     M.post('media', {file: fs.createReadStream(imageURL)}).then(resp => {
         const id = resp.data.id;
         M.post('statuses', {status: imageText, media_ids: [id]})
     });
+
+    console.log(`Posted media`);
 
 })()
 
