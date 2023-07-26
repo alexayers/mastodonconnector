324709ad8b03e22402c88190fd7ad775b38ccd2c
alexayers <ayers.alex@gmail.com>
2023-07-26 11:45:42 -0400

    Initial commit

--- pixelfed/src/app/main.ts
+++ pixelfed/src/app/main.ts
@@ -10,11 +10,22 @@ import * as fs from "fs";
     const PIXELFED_STORAGE_ROOT = config.pixelFed.baseStorage;
 
     for (let accountIdx = 0; accountIdx < config.pixelFed.sync.length; accountIdx++) {
+
         const MASTODON_API = config.pixelFed.sync[accountIdx]["to"]["api"];
         const accountID = config.pixelFed.sync[accountIdx].from["account"];
         const response = await axios.get(`${PIXELFED_API}/${accountID}/statuses?limit=1`)
         const imageText = response.data[0]["content_text"];
         const imageURL = PIXELFED_STORAGE_ROOT + response.data[0]["media_attachments"][0]["url"].split("storage")[1];
+
+        fs.open(accountID,'r',function(err, fd){
+            if (err) {
+                fs.writeFile(accountID, '-1', function(err) {
+                    if(err) {
+                        console.log(err);
+                    }
+                });
+            }
+        });
 
         console.log(`Running for account ${accountID} fetching ${imageURL}`);
 
@@ -31,23 +42,13 @@ import * as fs from "fs";
         M.post('media', {file: fs.createReadStream(imageURL)}).then(resp => {
             const id = resp.data.id;
 
-            fs.open(accountID,'r',function(err, fd){
-                if (err) {
-                    fs.writeFile(accountID, '-1', function(err) {
-                        if(err) {
-                            console.log(err);
-                        }
-                    });
-                }
-            });
-
             let contents = fs.readFileSync(accountID, {encoding: 'utf8', flag: 'r'});
 
-            if (contents != id) {
+            if (contents != imageURL) {
                 console.log(`Sending image with status ${imageText}`);
 
                 M.post('statuses', {status: `${imageText} \nCross Posted from PixelFed :-)`, media_ids: [id]})
-                fs.writeFileSync(accountID, id,{ encoding: 'utf8', flag: 'w' });
+                fs.writeFileSync(accountID, imageURL,{ encoding: 'utf8', flag: 'w' });
             } else {
                 console.log("Skipping this post as it's already been cross posted");
             }
